ba3a107a2e8fb233e5de4fa7af5ef3cdfe6f6408
Ender Doe <ender@shpazzle.com>
2020-08-21 19:45:32 -0400

    Update SolunaAlgorithm.cpp

--- SolunaAlgorithm.cpp
+++ SolunaAlgorithm.cpp
@@ -6,6 +6,14 @@
 #include <assert.h> 
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
+// copy of boosts hash_combine
+template <class T>
+inline void hash_combine(std::size_t& s, const T& v)
+{
+    std::hash<T> h;
+    s ^= h(v) + 0x9e3779b9 + (s << 6) + (s >> 2);
+}
 
 enum PieceId { Sun = 0, Moon = 1, ShootingStar = 2, Stars = 3 };
 #define COLOR_COUNT 4u
@@ -36,6 +44,48 @@ struct BranchResult {
     // move set
 } typedef BranchResult;
 
+
+struct GameStateHash
+{
+    size_t operator()(const GameState& gameState) const {
+        size_t hsh = std::hash<uint32_t>{}(gameState.colorCount);
+        for (uint32_t i = 0; i < COLOR_COUNT; ++i) {            
+            for (uint32_t j = 0; j < gameState.pieceLists[i].size(); ++j) {
+                hash_combine<uint32_t>(hsh, gameState.pieceLists[i][j].height);
+                hash_combine<uint32_t>(hsh, gameState.pieceLists[i][j].count);
+            }
+        }
+
+        return hsh;
+    }
+};
+
+struct ColorPieceVector {
+    size_t operator()(const std::vector<PieceStack> &pieceList) const {
+        size_t hsh = std::hash<uint32_t>{}(pieceList.size());
+        for (uint32_t i = 0; i < pieceList.size(); ++i) {
+            hash_combine<uint32_t>(hsh, pieceList[i].count);
+            hash_combine<uint32_t>(hsh, pieceList[i].height);
+        }
+
+        return 0;
+    }
+};
+
+bool operator==(const GameState& gameState1, const GameState& gameState2) {
+    if (gameState1.colorCount != gameState2.colorCount) return false;
+    for (uint32_t i = 0; i < COLOR_COUNT; ++i) {
+        if (gameState1.pieceLists[i].size() != gameState2.pieceLists[i].size()) return false;
+        for (uint32_t j = 0; j < gameState1.pieceLists[i].size(); ++j) {
+            if (gameState1.pieceLists[i][j].height != gameState2.pieceLists[i][j].height || gameState1.pieceLists[i][j].count != gameState2.pieceLists[i][j].count) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 MoveResult returnMove;
 std::string enumNames[] = {"Sun", "Moon", "Shooting Star", "Stars" };
 
@@ -62,7 +112,6 @@ std::vector<std::vector<uint32_t>> getCombinations(uint32_t n, uint32_t r) {
     return result;
 }
 
-
 std::vector<std::vector<uint32_t>> nextPartition(std::vector<std::vector<uint32_t>> a, uint32_t n)
 {
     std::vector<std::vector<uint32_t>> result = std::vector<std::vector<uint32_t>>();
@@ -86,6 +135,31 @@ std::vector<std::vector<uint32_t>> nextPartition(std::vector<std::vector<uint32_
     }
     result.push_back({ n });
     return result;
+}
+
+std::unordered_map<std::vector<PieceStack>, uint32_t, ColorPieceVector> generatePartitionId()
+{
+    std::unordered_map<std::vector<PieceStack>, uint32_t, ColorPieceVector> colorListToPartition = std::unordered_map < std::vector<PieceStack>, uint32_t, ColorPieceVector>();
+
+    std::vector<std::vector<uint32_t>> heightCountPartition = std::vector<std::vector<uint32_t>>();
+    uint32_t partitionId = 0;
+    for (uint32_t i = 1; i <= PIECE_COUNT; ++i) {
+        heightCountPartition = nextPartition(heightCountPartition, i);
+        for (uint32_t j = 0; j < heightCountPartition.size(); ++j) {
+            std::vector<PieceStack> pieces = std::vector<PieceStack>();
+            uint32_t nmbIdx = 0;
+            for (uint32_t k = 0; k < heightCountPartition[j].size(); ++k) {                
+                if (heightCountPartition[j][k] != heightCountPartition[j][nmbIdx]) {
+                    pieces.push_back({ heightCountPartition[j][k], (k - nmbIdx) });
+                    nmbIdx = k;
+                }                
+            }
+            colorListToPartition.insert({ pieces, partitionId });
+            ++partitionId;
+        }        
+    }
+
+    return colorListToPartition;
 }
 
 GameState colorPartitionVectorToGameState(std::vector<uint32_t>& colorPartition, uint16_t height) {
@@ -141,22 +215,17 @@ std::vector<GameState> getBoardHeightSpaceSubColorSpace(std::vector<uint32_t> bo
 
         std::vector<GameState> nextResult = std::vector<GameState>();
 
-       
         for (uint32_t a = 0; a < mergingColorPartition.size(); ++a) {
             std::vector<std::vector<uint32_t>> combinations = getCombinations(COLOR_COUNT, mergingColorPartition[a].size());
             for (uint32_t j = 0; j < result.size(); ++j) {
                 for (uint32_t k = 0; k < combinations.size(); ++k) {
                     GameState newGameState = result[j];                   
                     for (uint32_t l = 0; l < combinations[k].size(); ++l) {                     
-                            newGameState.pieceLists[l].push_back({ (uint16_t)currentHeight, mergingColorPartition[a][l] });             
+                            newGameState.pieceLists[combinations[k][l]].push_back({ (uint16_t)currentHeight, mergingColorPartition[a][l] });
                     }
                     // recount colors
                     for (uint32_t l = 0; l < COLOR_COUNT; ++l) {
-                        if (newGameState.pieceLists[l].size() == 0) {
-                            newGameState.colorCount = l;
-                            break;
-                        }
-                        if (l == COLOR_COUNT - 1)newGameState.colorCount = COLOR_COUNT;
+                        if (newGameState.pieceLists[l].size() != 0) ++newGameState.colorCount;
                     }
                     nextResult.push_back(newGameState);
                 }
@@ -168,7 +237,7 @@ std::vector<GameState> getBoardHeightSpaceSubColorSpace(std::vector<uint32_t> bo
     return result;
 }
 
-std::vector<uint32_t> getSymmetries(GameState & gameState) {
+std::vector<uint32_t> getSymmetries(const GameState & gameState) {
 
     std::vector<uint32_t> result = std::vector<uint32_t>(COLOR_COUNT);
 
@@ -195,7 +264,7 @@ std::vector<uint32_t> getSymmetries(GameState & gameState) {
     return result;
 }
 
-std::vector<MoveResult> getPossibleMoves(GameState & gameState) {
+std::vector<MoveResult> getPossibleMoves(const GameState & gameState) {
 
     std::vector<MoveResult> result = std::vector<MoveResult>();
     std::vector<uint32_t> symmetries = getSymmetries(gameState);
@@ -236,14 +305,14 @@ std::vector<MoveResult> getPossibleMoves(GameState & gameState) {
         if (indicesExhausted) exit = true;
         else {
             for (uint32_t i = 0; i < sameHeightsColorIndex.size(); ++i) {
-                if (symmetries[sameHeightsColorIndex[i]] != i) continue;
+                if (symmetries[sameHeightsColorIndex[i]] != sameHeightsColorIndex[i]) continue;
                 std::unordered_set<uint32_t> dupSet = std::unordered_set<uint32_t>();
                 for (uint32_t j = i + 1; j < sameHeightsColorIndex.size(); ++j)
                 {
-                    if (dupSet.count(symmetries[sameHeightsColorIndex[i]]) == 1) continue;
-                    dupSet.insert(symmetries[sameHeightsColorIndex[i]]);
+                    if (dupSet.count(symmetries[sameHeightsColorIndex[j]]) == 1) continue;
+                    dupSet.insert(symmetries[sameHeightsColorIndex[j]]);
                     result.push_back({ sameHeightsColorIndex[i], sameHeightsSubIndex[i], sameHeightsColorIndex[j], sameHeightsSubIndex[j] });
-                    if (symmetries[sameHeightsColorIndex[i]] != i) {
+                    if (symmetries[sameHeightsColorIndex[j]] != sameHeightsColorIndex[i]) {
                         result.push_back({ sameHeightsColorIndex[j], sameHeightsSubIndex[j], sameHeightsColorIndex[i], sameHeightsSubIndex[i] });
                     }
                 }
@@ -253,6 +322,8 @@ std::vector<MoveResult> getPossibleMoves(GameState & gameState) {
     } while (!exit);
     return result;
 }
+
+
 
 
 // MoveResult * getPossibleMoves(PieceStack* arr, uint32_t len, uint32_t * wLen) {
@@ -368,16 +439,22 @@ void getAllSymmertricBoardSpaces()
         heightPartition = nextPartition(heightPartition, i);
     }
 
-    std::vector<GameState> allGameStates = std::vector<GameState>();
+    std::unordered_set<GameState, GameStateHash> allGameStates = std::unordered_set<GameState, GameStateHash>();
     for (uint32_t i = 0; i < heightPartition.size(); ++i) {
+        // returns dupes!!!
         std::vector<GameState> gameStates = getBoardHeightSpaceSubColorSpace(heightPartition[i]);
-        for (uint32_t j = 0; j < gameStates.size(); ++j) {
-            allGameStates.push_back(gameStates[j]);
-        }
+        for (uint32_t j = 0; j < gameStates.size(); ++j)
+            if (allGameStates.insert(gameStates[j]).second == false) {
+                std::cout << std::endl;
+            }
     }
 
-    for (uint32_t i = 0; i < allGameStates.size(); ++i) {       
-        std::vector<MoveResult> moves = getPossibleMoves(allGameStates[i]);
+    std::unordered_map<GameState, std::vector<MoveResult>, GameStateHash> map = std::unordered_map<GameState, std::vector<MoveResult>, GameStateHash>();   
+    for (auto const& gameState : allGameStates) {
+        
+        std::vector<MoveResult> moves = getPossibleMoves(gameState);
+
+        map.insert({ gameState, moves });
     }
 }
 
@@ -473,7 +550,6 @@ void getAllSymmertricBoardSpaces()
 
 int main()
 {
-    getCombinations(4, 3);
     getAllSymmertricBoardSpaces();
     return 0;
     //{
